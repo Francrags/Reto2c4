790b0d1ef3d0d9b94d54d2b1c6528f259ad80db0
Rags <soto220512@gmail.com>
2021-12-15 22:08:59 -0500

    Reto2WebApplication

--- src/main/java/Reto2_Web/Reto2WebApplication.java
+++ src/main/java/Reto2_Web/Reto2WebApplication.java
@@ -1,13 +1,28 @@
 package Reto2_Web;
 
+import Reto2_Web.interfaces.InterfaceCleaningProduct;
+import Reto2_Web.interfaces.InterfaceUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Reto2WebApplication {
+public class Reto2WebApplication implements CommandLineRunner {
+
+    @Autowired
+    private InterfaceCleaningProduct InterfaceCleaningProduct;
+    @Autowired
+    private InterfaceUser interfaceUser;
 
     public static void main(String[] args) {
         SpringApplication.run(Reto2WebApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        InterfaceCleaningProduct.deleteAll();
+        interfaceUser.deleteAll();
     }
 
 }
